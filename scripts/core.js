b4d4a2ca0aa7aeffcb7a63bfe13b0d86bce98ff7
SexyJackXy <tjachmann26@gmail.com>
2026-09-14 06:45:13 +0200

    Bugfix: Fixed the Reset Bug
    I had a Bug that I can't reset the plans. This was cause by the bug that I forgott th import the dragAndDrop.js to the settings.html

--- scripts/core.js
+++ scripts/core.js
@@ -339,14 +339,17 @@
     importNotWorkingPeople()
   }
 
-  var dnd = window.DragAndDrop.createDragAndDrop({
-    reservedNames,
-    scheduleSave,
-    temporaryScheduleSave,
-    activityScheduleSave,
-    exportNotWorkingPeople,
-    removeFromSchedule
-  })
+  var dnd = window.DragAndDrop
+    ? window.DragAndDrop.createDragAndDrop({
+      reservedNames,
+      scheduleSave,
+      temporaryScheduleSave,
+      activityScheduleSave,
+      exportNotWorkingPeople,
+      removeFromSchedule
+    })
+    : { initDragAndDrop: () => { }, updatePersonColor: () => { } }
+
   var initDragAndDrop = dnd.initDragAndDrop
   var updatePersonColor = dnd.updatePersonColor
 
